27412e49c7d9cd1dc440b0a7dbe77d491e578410
Kjell-Magne Øierud <kjellm@oierud.net>
2015-11-15 10:00:24 +0100

    Run data gathering in threads

--- bev.rb
+++ bev.rb
@@ -30,13 +30,14 @@ def reset_data
   $data = Struct::Data.new(*Array.new($metrics.length, 0))
 end
 
-def get(uri, req_proc)
+def get(uri, req_proc, res_proc)
   Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
     req = Net::HTTP::Get.new uri
     req_proc.(req)
     response = http.request req
     raise response.error! unless response.code == "200"
-    body = JSON.parse response.body
+
+    body = JSON.parse res_proc.(response)
     if block_given?
       yield body
     else
@@ -49,36 +50,21 @@ def github_get(uri, &block)
   get(uri, ->(req) {
         req["Accept"] = "application/vnd.github.v3+json"
         req['Authorization'] = "token #$github_token"
-      }, &block)
+      }, ->(res) {res.body}, &block)
 end
 
-def heroku_get(uri)
-  http = Net::HTTP.new uri.host, uri.port
-  http.use_ssl = true
-  #http.set_debug_output $stderr
-  http.start do |http|
-    req = Net::HTTP::Get.new uri
-    req["Accept"] = "application/vnd.heroku+json; version=3"
-    req['Authorization'] = "Bearer #$heroku_token"
-
-
-
-    response = http.request req
-    raise response.error! unless response.code == "200"
-
-    # FIXME why?
-    body = if response.key?("content-encoding")
-             Zlib::GzipReader.new(StringIO.new(response.body)).read
-           else
-             response.body
-           end
-    body = JSON.parse body
-    if block_given?
-      yield body
-    else
-      body
-    end
-  end
+def heroku_get(uri, &block)
+  get(uri, ->(req) {
+        req["Accept"] = "application/vnd.heroku+json; version=3"
+        req['Authorization'] = "Bearer #$heroku_token"
+      }, ->(res) {
+        # FIXME why?
+        if res.key?("content-encoding")
+          Zlib::GzipReader.new(StringIO.new(res.body)).read
+        else
+          res.body
+        end
+      }, &block)
 end
 
 def snapci_get(uri)
@@ -149,78 +135,91 @@ def print_banner
 EOT
 end
 
-def main
-  reset_data
-
-  # GITHUB
-
-  repos_url = github_get URI("https://api.github.com/orgs/gramo-org") do |resp|
-    resp["repos_url"]
-  end
-
-  github_repos = github_get URI(repos_url)
-
-  github_repos.each do |repo|
-    url = repo['issues_url'].sub(/\{\/number\}/, '')
-    github_get URI(url) do |resp|
-      $data.in_progress += resp.count do |r|
-        r["labels"].find {|l| l["name"] == "in progress" } \
-        && r["labels"].find {|l| l["name"] == "feature" }
-      end
-    end
-
-    url = repo['pulls_url'].sub(/\{\/number\}/, '')
-    github_get URI(url) do |resp|
-      $data.pull_requests += resp.count
-    end
-  end
-
-# HEROKU
-
-  apps = heroku_get URI("https://api.heroku.com/apps") do |resp|
-    resp.find_all { |app| app["name"].start_with? "gramo" }
-  end
-
-  apps.each do |app|
-    uri = URI("https://api.heroku.com/apps/#{app["name"]}/dynos")
-    heroku_get uri  do |resp|
-      $data.dynos_down += resp.count { |d| not %w(idle up).include? d["state"] }
-    end
-  end
-
-
-# SNAP CI
-
-  github_repos.each do |repo|
-    latest_url = begin
-                   snapci_get URI("https://api.snap-ci.com/project/#{repo['full_name']}/branch/master/pipelines/latest") do |resp|
-                     resp["_links"]["redirect"]["href"]
-                   end
-                 rescue
-                   next
-                 end
-
-    snapci_get URI(latest_url) do |resp|
-      $data.failing_pipelines += 1 if resp['result'] != 'passed'
-    end
-  end
-
-  #$data.todos = `ack -hc --ignore-dir=.bundle --ignore-dir=bower_components --ignore-dir=dist --ignore-dir=tmp "FIX|TODO"`
-
-  write_summary_to_file
-  system("clear")
-  print_banner
+def print_metrics
   print_metric "Stories in progress", :in_progress
   print_metric "Pull requests",       :pull_requests
   print_metric "Dynos down",          :dynos_down
   print_metric "Failed pipelines",    :failing_pipelines
-
+  print_metric "Todo comments",       :todos
 end
 
-system("clear")
-reset_data
-write_summary_to_file # Useful for processes that expect the file to exists
-print_banner
+def gather_data
+  threads = []
+
+  # GITHUB
+  repos_url = github_get URI("https://api.github.com/orgs/gramo-org") do |resp|
+    resp["repos_url"]
+  end
+  github_repos = github_get URI(repos_url)
+
+  threads << Thread.new do
+    github_repos.each do |repo|
+      url = repo['issues_url'].sub(/\{\/number\}/, '')
+      github_get URI(url) do |resp|
+        $data.in_progress += resp.count do |r|
+          r["labels"].find {|l| l["name"] == "in progress" } \
+          && r["labels"].find {|l| l["name"] == "feature" }
+        end
+      end
+
+      url = repo['pulls_url'].sub(/\{\/number\}/, '')
+      github_get URI(url) do |resp|
+        $data.pull_requests += resp.count
+      end
+    end
+  end
+
+  # HEROKU
+  threads << Thread.new do
+    apps = heroku_get URI("https://api.heroku.com/apps") do |resp|
+      resp.find_all { |app| app["name"].start_with? "gramo" }
+    end
+
+    apps.each do |app|
+      uri = URI("https://api.heroku.com/apps/#{app["name"]}/dynos")
+      heroku_get uri  do |resp|
+        $data.dynos_down += resp.count { |d| not %w(idle up).include? d["state"] }
+      end
+    end
+  end
+
+
+  # SNAP CI
+  threads << Thread.new do
+    github_repos.each do |repo|
+      latest_url = begin
+                     snapci_get URI("https://api.snap-ci.com/project/#{repo['full_name']}/branch/master/pipelines/latest") do |resp|
+                       resp["_links"]["redirect"]["href"]
+                     end
+                   rescue
+                     next
+                   end
+
+      snapci_get URI(latest_url) do |resp|
+        $data.failing_pipelines += 1 if resp['result'] != 'passed'
+      end
+    end
+  end
+
+  # TODOS
+  threads << Thread.new do
+    ignore = %w(.bundle bower_components dist tmp)
+    ignore = [''].concat(ignore).join(" --ignore-dir=")
+    $data.todos = `ack -hc #{ignore} "FIX|TODO"`.to_i
+  end
+
+  threads.each { |thread| thread.join }
+end
+
+def main
+  reset_data
+  write_summary_to_file
+  system("clear")
+  print_banner
+  gather_data
+  print_metrics
+end
+
 while true do
   main
   sleep 60
